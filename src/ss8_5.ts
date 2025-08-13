539a23d38df5173999c9ccd316ad5442fbe380db
Nguyễn Mạnh Hùng <fc.canhcut1@gmail.com>
2025-08-13 23:28:30 +0700

    second commit

--- src/ss8_5.ts
+++ src/ss8_5.ts
@@ -1,3 +1,3 @@
-const findMatch = <type>(arr: type[], predicate: (item: type) => boolean): type | undefined => arr.find(item => typeof item == "number" && item % 2 == 0 && predicate(item))
+const findMatch = <type>(arr: type[]): type | undefined => arr.find(item => typeof item == "number" && item % 2 == 0)
 const numbers = [1, 2, 3, 4, 5]
-console.log(findMatch(numbers, n => n > 3))
+console.log(findMatch(numbers))
